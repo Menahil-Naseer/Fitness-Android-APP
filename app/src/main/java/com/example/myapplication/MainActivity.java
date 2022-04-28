2f2970e5d8471d3169e8d5b30e466eb5b5a7c640
Menahil-Naseer <menahilnaseer436@gmail.com>
2022-04-28 14:56:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.myapplication;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public FirebaseAuth mAuth;
+    Button signUpButton;
+    EditText signUpEmailTextInput;
+    EditText signUpPasswordTextInput;
+    CheckBox agreementCheckBox;
+    TextView errorView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpEmailTextInput = findViewById(R.id.editTextTextEmailAddress);
+        signUpPasswordTextInput = findViewById(R.id.editTextTextPassword);
+        signUpButton = findViewById(R.id.signup_btn);
+        agreementCheckBox = findViewById(R.id.checkBox);
+        errorView = findViewById(R.id.textView2);
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (signUpEmailTextInput.getText().toString().contentEquals("")) {
+
+
+                    errorView.setText("Email cannot be empty");
+
+
+                } else if (signUpPasswordTextInput.getText().toString().contentEquals("")) {
+
+
+                    errorView.setText("Password cannot be empty");
+
+
+                } else if (!agreementCheckBox.isChecked()) {
+
+                    errorView.setText("Please agree to terms and Condition");
+
+
+                } else {
+
+
+                    mAuth.createUserWithEmailAndPassword(signUpEmailTextInput.getText().toString(), signUpPasswordTextInput.getText().toString()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                try {
+                                    if (user != null)
+                                        user.sendEmailVerification()
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                                                                    MainActivity.this);
+
+                                                            // set title
+                                                            alertDialogBuilder.setTitle("Thankyou!");
+
+                                                            // set dialog message
+                                                            alertDialogBuilder
+                                                                    .setMessage("Signup Successful!...")
+                                                                    .setCancelable(false)
+                                                                    .setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+                                                                        public void onClick(DialogInterface dialog, int id) {
+
+                                                                            Intent in = new Intent(MainActivity.this, SignInActivity.class);
+                                                                            startActivity(in);
+                                                                        }
+                                                                    });
+
+                                                            // create alert dialog
+                                                            AlertDialog alertDialog = alertDialogBuilder.create();
+
+                                                            // show it
+                                                            alertDialog.show();
+
+
+                                                        }
+                                                    }
+                                                });
+
+                                } catch (Exception e) {
+                                    errorView.setText(e.getMessage());
+                                }
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                                if (task.getException() != null) {
+                                    errorView.setText(task.getException().getMessage());
+                                }
+
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SignInActivity.java
@@ -0,0 +1,108 @@
+package com.example.myapplication;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignInActivity";
+    public FirebaseAuth mAuth;
+    EditText emailTextInput;
+    EditText passwordTextInput;
+    Button signInButton;
+    Button forgotPasswordButton;
+    TextView errorView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        emailTextInput = findViewById(R.id.editTextTextEmailAddress2);
+        passwordTextInput = findViewById(R.id.editTextTextPassword2);
+        signInButton = findViewById(R.id.signin_btn);
+        errorView = findViewById(R.id.error);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (emailTextInput.getText().toString().contentEquals("")) {
+
+
+                    errorView.setText("Email cant be empty");
+
+
+                } else if (passwordTextInput.getText().toString().contentEquals("")) {
+
+                    errorView.setText("Password cant be empty");
+
+                } else {
+
+
+                    mAuth.signInWithEmailAndPassword(emailTextInput.getText().toString(), passwordTextInput.getText().toString())
+                            .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        if (user != null) {
+
+                                                Intent HomeActivity = new Intent(SignInActivity.this, MainActivity3.class);
+                                                setResult(RESULT_OK, null);
+                                                startActivity(HomeActivity);
+                                                SignInActivity.this.finish();
+
+                                        }
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        if (task.getException() != null) {
+                                            errorView.setText(task.getException().getMessage());
+                                        }
+
+                                    }
+
+                                }
+                            });
+
+
+                }
+
+
+            }
+        });
+
+
+
+    }
+}
